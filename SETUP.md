4d283d5095b236a1562fd687c54b3faffebe84e3
Angus Mark <ngsmrk@gmail.com>
2013-09-10 18:24:12 +0100

    1st tests with artoo API

---
+++ SETUP.md
@@ -0,0 +1,9 @@
+### Setup
+
+## Gems
+
+gem install artoo
+
+gem install hybridgroup-serialport
+
+gem install artoo-pebble
---
+++ src/hello_world.rb
@@ -0,0 +1,12 @@
+require 'artoo'
+
+connection :loop
+
+work do
+  every(3.seconds) do
+    puts "hello"
+  end
+  after(10.seconds) do
+    puts "wow"
+  end
+end
