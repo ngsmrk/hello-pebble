5be58c9e69bdbf5d6fa16c6f830aa5e8275b89a3
Angus Mark <ngsmrk@gmail.com>
2013-09-11 13:24:27 +0100

    1st version of pebble program

---
+++ src/hello_pebble.rb
@@ -0,0 +1,15 @@
+require "pebble"
+
+# Create your watch using the serial port assigned to your Pebble.
+#watch = Pebble::Watch.new("7F30", "/dev/tty.Pebble7F30-SerialPortSe")
+# You can also use autodetection if you're on OS X:
+watch = Pebble::Watch.autodetect
+
+watch.connect
+
+watch.ping
+
+at_exit do
+  puts watch
+  watch.disconnect if watch
+end
