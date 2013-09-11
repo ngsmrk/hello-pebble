2593f4b5b3ffb98ad1eadbdf79bb18662aa4da78
Angus Mark <ngsmrk@gmail.com>
2013-09-11 13:47:42 +0100

    Clean up

--- src/pebble_event_listener.rb
+++ src/pebble_event_listener.rb
@@ -9,6 +9,29 @@ watch.protocol.on_receive do |message|
   puts message
 end
 
+watch.on_event(:log) do |event|
+    puts "Log event: #{event}"
+end
+
+watch.on_event(:system_message) do |event|
+  puts "System message event: #{event}"
+end
+
+watch.on_event(:app_message) do |event|
+  puts "App message event: #{event}"
+end
+
+watch.on_event(:media_control) do |event|
+  case event.button
+    when :playpause
+      puts "Play or pause music"
+    when :next
+      puts "Next track"
+    when :previous
+      puts "Previous track"
+  end
+end
+
 watch.connect
 
 watch.listen_for_events
